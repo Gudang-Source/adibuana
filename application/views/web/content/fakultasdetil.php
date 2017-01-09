8142d1f86e1ac4f93d00252eafdfbd3d9ec3cd51
naynayla23 <nayla.aisyah4@gmail.com>
2017-01-09 15:38:45 +0700

    edit template

--- application/views/web/content/fakultasdetil.php
+++ application/views/web/content/fakultasdetil.php
@@ -3,15 +3,66 @@
         <section class="page-section">
           <div class="container">
             <div class="row">
-              <div class="col-md-7 section-block-p">
+              <div class="col-md-9 section-block-p">
                 <header class="hr-header">
-                  <h2 class="smb">WELCOME TO MEGATRON</h2>
-                  <p class="common-serif __caption">We've had the privilege to work with some awesome clients</p>
+                  <h2 class="smb">Bimbingan dan Konseling</h2>
+                  <p class="common-serif __caption">Detail Program Studi</p>
                   <div class="separator-2-color"></div>
                 </header>
-                <p>Megatron is a Multipurpose HTML 5 Template with which responds to the most demanding customers. It can be a great choice for your Corporate, Ecommerce, Portfolio, Creative Agency etc. This theme can easily satisfy all of your needs. Use Megatron template to create any interactive website you want. Megatron build with responsive design, meaning your new site will adapt to fit any screen size, from desktops down to mobile phones. All pages and features have been optimized to work on every device. It’s one thing to develop a responsive theme.</p>
+                <h4>PENDIDIKAN GURU BIMBINGAN KONSELING / BK (S-1 TERAKREDITASI A) 1151/SK/BAN-PT/Akred/S/XI/2015</h4>
+                	<p>Program studi S-1 Bimbingan dan Konseling (BK) merupakan program studi tertua di lingkungan Fakultas Keguruan dan Ilmu Pendidikan (FKIP) Universitas PGRI Adi Buana Surabaya. Semula program studi ini berasal dari Jurusan Psikologi Pendidikan dan Bimbingan (PBB), pada tahun 1998 ketika IKIP PGRI Surabaya menjadi Universitas PGRI Adi Buana Surabaya setelah marger dengan Sekolah Tinggi Teknik Lingkungan Adi Buana Surabaya (STTL), Jurusan PBB menjadi program studi Bimbingan dan Konseling yang ditetapkan dengan Surat Keputusan Menteri Pendidikan dan Kebudayaan RI Nomor:47/D/O/1998</p>
+                <h4>Visi</h4>
+                	<p>Pada tahun 2025 menghasilkan sumberdaya manusia berbasis riset dan kewirausahaan, berwawasan global, adaptif dalam bidang Bimbingan dan Konseling.</p>
+                <h4>Misi</h4>
+                	<p>1. Peningkatan penyelenggaraan pendidikan dan pengajaran dalam rangka mempersiapkan sumber daya manusia yang berkualitas tinggi.</p>
+	                <p>2. Mengembangkan kurikulum berbasis kompetensi yang kontekstual untuk memenuhi kebutuhan tenaga konselor dan kebutuhan stake holder.</p>
+					<p>3. Mengembangkan serta memajukan bimbingan dan konseling sebagai ilmu dan profesi yang bermartabat dengan cara melakukan peningkatkan kualitas penyelenggaraan penelitian dalam bidang Bimbingan dan Konseling khususnya bidang pendidikan.</p>
+					<p>4. Meningkatkan kualitas penyelenggaraan pengabdian kepada masyarakat berbasis research dalam rangka pemberdayaan sumber daya manusia.</p>
+					<p>5. Mengembangkan pembelajaran berbasis IPTEKS, dan enterpreneurship sebagai sarana bagi kesiapan lulusan memasuki dunia kerja serta mengembangan diri.</p>
+					<p>6. Meningkatkan kualitas Sistem Tata Pamong pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya.</p>
+					<p>7. Meningkatkan kualitas mahasiswa dan lulusan pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya</p>
+					<p>8. Meningkatkan kualitas Sumber Daya Manusia pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya.</p>
+					<p>9. Meningkatkan kualitas Kurikulum pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya.</p>
+					<p>10. Mengembangkan sarana dan prasarana pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya.</p>
+					<p>11. Meningkatkan kualitas dan kuantitas Penelitian dan Pengabdian kepada Masyarakat pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya.</p>
+					<p>12. Meningkatkan kualitas dan kuantitas kerjasama baik regional, nasional dan internasional pada Program Studi Bimbingan dan Konseling FKIP Universitas PGRI Adi Buana Surabaya</p>
+
+				<p class="common-serif __caption">Fasilitas</p>
+				<div class="separator-2-color"></div>
+				<p>Program studi BK dilengkapi dengan fasilitas-fasilitas seperti:</p>
+				<p>Ruang kuliah ber-AC, Laboratorium Bimbingan dan Konseling yang dilengkapi dengan instrumen tes psikologis (tes inteligensi, tes bakat,tes minat, tes kepribadian, dan tes kreativitas), studio Micro-Counseling, ruang observasi dan diskusi.</p>
+
+				<p class="common-serif __caption">Kerja Sama</p>
+				<div class="separator-2-color"></div>
+				<p>Dalam implementasi Tridharma Perguruan Tinggi, program studi Bimbingan dan konseling (BK) melakukan kerja sama dengan institusi/organisasi sebagai berikut :</p>
+				<p>1. Perguruan Tinggi dalam dan luar negeri dalam pengembangan Sumber Daya Manusia (SDM) khususnya dosen dan mahasiswa.<br>
+				2. Asosiasi Bimbingan dan Konseling Indonesia (ABKIN) dalam pengembangan kurikulum dan peningkatan kompetensi dosen.<br>
+				3. SMP dan SMA di wilayah Provinsi Jawa Timur dalam pelaksanakan Program Pengalaman Lapangan (PPL) Bimbingan dan Konseling.<br>
+				4. Musyawarah Guru Bimbingan dan Konseling (MGBK) SMP dan SMA di lingkungan Dinas pendidikan dan Kementrian Agama dalam penyelengaraan kegiatan Pengembangan Keprofesian Berkelanjutan (PKB) dalam bentuk pelatihan Penelitian Tindakan Kelas Bimbingan dan Konseling (PTK-BK), pelatihan konseling, workshop, dan seminar nasional.</p>
+
+				<p class="common-serif __caption">Prospek Lulusan</p>
+				<div class="separator-2-color"></div>
+				<p>Menjadi guru BK/konselor di SMP, SMA, SMK dan yang sederajat, konselor industri, konsultan pendidikan, dan peneliti dalam bidang pendidikan.</p>
               </div>
-              <div class="col-md-5 section-block pt-20"><a href="__modal-video.html" data-modal-open><img src="assets/images/other/about-video-thumbnail.jpg" alt="Video Thumbnail" class="img-responsive"></a>
+
+              <div class="col-md-3 col-sm-6">
+                <div class="widget widget-categories">
+                  <ul class="__widget-content">
+                    <li><a href="#">Bimbingan dan Konseling</a></li>
+                    <li><a href="#">Pendidikan Kesejahteraan Keluarga</a></li>
+                    <li><a href="#">Pendidikan Moral Pancasila dan Kewarganegaraan</a></li>
+                    <li><a href="#">Pendidikan Bahasa dan Sastra Indonesia</a></li>
+                    <li><a href="#">Pendidikan Matematika</a></li>
+                    <li><a href="#">Pendidikan Bahasa Inggris</a></li>
+                    <li><a href="#">Pendidikan Kepelatihan Olahraga</a></li>
+                    <li><a href="#">Pendidikan Seni Rupa</a></li>
+                    <li><a href="#">Pendidikan Guru Sekolah Dasar</a></li>
+                    <li><a href="#">Pendidikan Guru - PAUD</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              </div>
               </div>
             </div>
           </div>
